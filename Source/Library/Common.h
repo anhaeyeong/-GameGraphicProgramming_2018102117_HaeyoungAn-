b3bf0211f1783ede3249592431fc6636b6d5bce6
anhaeyeong <ahy3809@gmail.com>
2022-04-03 03:04:10 +0900

    [LAB03] lab03 submitted
    fucking lab

--- Source/Library/Common.h
+++ Source/Library/Common.h
@@ -10,7 +10,7 @@
   © 2022 Kyung Hee University
 ===================================================================+*/
 #pragma once
-
+#pragma comment(lib, "d3dcompiler.lib")
 #ifndef  UNICODE
 #define UNICODE
 #endif // ! UNICODE
@@ -19,6 +19,7 @@
 #include <wrl.h>
 
 #include <d3d11_4.h>
+#include <d3dcompiler.h>
 #include <directxcolors.h>
 
 #include <memory>
--- Source/Library/Renderer/Renderer.cpp
+++ Source/Library/Renderer/Renderer.cpp
@@ -7,9 +7,10 @@ namespace library
 
       Summary:  Constructor
 
-      Modifies: [m_driverType, m_featureLevel, m_d3dDevice, m_d3dDevice1, 
-                  m_immediateContext, m_immediateContext1, m_swapChain, 
-                  m_swapChain1, m_renderTargetView].
+      Modifies: [m_driverType, m_featureLevel, m_d3dDevice, m_d3dDevice1,
+                  m_immediateContext, m_immediateContext1, m_swapChain,
+                  m_swapChain1, m_renderTargetView, m_vertexShader,
+                  m_pixelShader, m_vertexLayout, m_vertexBuffer].
     M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
     /*--------------------------------------------------------------------
       TODO: Renderer::Renderer definition (remove the comment)
@@ -28,9 +29,10 @@ namespace library
       Args:     HWND hWnd
                   Handle to the window
 
-      Modifies: [m_d3dDevice, m_featureLevel, m_immediateContext, 
-                  m_d3dDevice1, m_immediateContext1, m_swapChain1, 
-                  m_swapChain, m_renderTargetView].
+      Modifies: [m_d3dDevice, m_featureLevel, m_immediateContext,
+                  m_d3dDevice1, m_immediateContext1, m_swapChain1,
+                  m_swapChain, m_renderTargetView, m_vertexShader, 
+                  m_vertexLayout, m_pixelShader, m_vertexBuffer].
 
       Returns:  HRESULT
                   Status code
@@ -115,7 +117,7 @@ namespace library
         }
         if (FAILED(hr))
             return hr;
-        
+
         // Create swap chain
         ComPtr<IDXGIFactory2> dxgiFactory2;
         hr = dxgiFactory.As(&dxgiFactory2);
@@ -128,14 +130,14 @@ namespace library
                 m_immediateContext.As(&m_immediateContext1);
             }
 
-            DXGI_SWAP_CHAIN_DESC1 sd = {};
-            sd.Width = width;
-            sd.Height = height;
-            sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-            sd.SampleDesc.Count = 1;
-            sd.SampleDesc.Quality = 0;
-            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-            sd.BufferCount = 1;
+            DXGI_SWAP_CHAIN_DESC1 sd = {
+            .Width = width,
+            .Height = height,
+            .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
+            .SampleDesc = { .Count = 1, .Quality = 0 },
+            .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+            .BufferCount = 1
+            };
             hr = dxgiFactory2->CreateSwapChainForHwnd(m_d3dDevice.Get(), hWnd, &sd, nullptr, nullptr, m_swapChain1.GetAddressOf());
             if (SUCCEEDED(hr))
             {
@@ -145,18 +147,23 @@ namespace library
         else
         {
             // DirectX 11.0 systems
-            DXGI_SWAP_CHAIN_DESC sd = {};
-            sd.BufferCount = 1;
-            sd.BufferDesc.Width = width;
-            sd.BufferDesc.Height = height;
-            sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-            sd.BufferDesc.RefreshRate.Numerator = 60;
-            sd.BufferDesc.RefreshRate.Denominator = 1;
-            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-            sd.OutputWindow = hWnd;
-            sd.SampleDesc.Count = 1;
-            sd.SampleDesc.Quality = 0;
-            sd.Windowed = TRUE;
+            DXGI_SWAP_CHAIN_DESC sd = {
+            .BufferDesc = {
+                            .Width = width,
+                            .Height = height,
+                            .RefreshRate = {.Numerator = 60,
+                                            .Denominator = 1},
+                            .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
+                           },
+            .SampleDesc = {
+                    .Count = 1,
+                    .Quality = 0
+                           },
+            .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+            .BufferCount = 1,
+            .OutputWindow = hWnd,
+            .Windowed = TRUE,
+            };
 
             hr = dxgiFactory->CreateSwapChain(m_d3dDevice.Get(), &sd, m_swapChain.GetAddressOf());
         }
@@ -183,6 +190,9 @@ namespace library
 
         m_immediateContext->OMSetRenderTargets(1, m_renderTargetView.GetAddressOf(), nullptr);
 
+        
+
+
         // Setup the viewport
         D3D11_VIEWPORT vp;
         vp.Width = (FLOAT)width;
@@ -193,6 +203,92 @@ namespace library
         vp.TopLeftY = 0;
         m_immediateContext->RSSetViewports(1, &vp);
 
+        ComPtr<ID3DBlob> pVSBlob;
+        hr = compileShaderFromFile(L"../Library/Shaders/Lab03.fxh", "VS", "vs_5_0", &pVSBlob);
+        if (FAILED(hr))
+        {
+            MessageBox(nullptr, L"FX VS file none", L"Error", MB_OK);
+            return hr;
+        }
+
+        
+
+        hr = m_d3dDevice->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, m_vertexShader.GetAddressOf());
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        // Define the input layout
+        D3D11_INPUT_ELEMENT_DESC aLayouts[] =
+        {
+            {
+                "POSITION",
+                0,
+                DXGI_FORMAT_R32G32B32_FLOAT,
+                0,
+                0,
+                D3D11_INPUT_PER_VERTEX_DATA,
+                0 },
+        };
+        UINT uNumElements = ARRAYSIZE(aLayouts);
+        hr = m_d3dDevice->CreateInputLayout(aLayouts, uNumElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), m_vertexLayout.GetAddressOf());
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        m_immediateContext->IASetInputLayout(m_vertexLayout.Get());
+
+
+        ComPtr<ID3DBlob> pPSBlob;
+        hr = compileShaderFromFile(L"../Library/Shaders/Lab03.fxh", "PS", "ps_5_0", &pPSBlob);
+        if (FAILED(hr))
+        {
+            MessageBox(nullptr, L"FX PS file none", L"Error", MB_OK);
+            return hr;
+        }
+        hr = m_d3dDevice->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, m_pixelShader.GetAddressOf());
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+        SimpleVertex aVertices[] =
+        {
+            { XMFLOAT3(0.0f, 0.5f, 0.5f) },
+            { XMFLOAT3(0.5f, -0.5f, 0.5f) },
+            { XMFLOAT3(-0.5f, -0.5f, 0.5f) }
+        };
+        D3D11_BUFFER_DESC bd = {
+        .ByteWidth = sizeof(SimpleVertex) * 3,
+        .Usage = D3D11_USAGE_DEFAULT,
+        .BindFlags = D3D11_BIND_VERTEX_BUFFER,
+        .CPUAccessFlags = 0,
+        .MiscFlags = 0,
+        };
+
+        D3D11_SUBRESOURCE_DATA initData = {
+            .pSysMem = aVertices,
+            .SysMemPitch = 0,
+            .SysMemSlicePitch = 0
+        };
+
+        hr = m_d3dDevice->CreateBuffer(&bd, &initData, m_vertexBuffer.GetAddressOf());
+        if (!m_vertexBuffer)
+        {
+            return E_FAIL;
+        }
+        if (FAILED(hr))
+        {
+            return hr;
+        }
+
+        UINT uStride = sizeof(SimpleVertex);
+        UINT uOffset = 0;
+
+        m_immediateContext->IASetVertexBuffers(0u, 1u, m_vertexBuffer.GetAddressOf(), &uStride, &uOffset);
+        m_immediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
         return S_OK;
     }
 
@@ -202,13 +298,83 @@ namespace library
       Summary:  Render the frame
     M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
     /*--------------------------------------------------------------------
-      TODO: Renderer::Initialize definition (remove the comment)
+      TODO: Renderer::Render definition (remove the comment)
     --------------------------------------------------------------------*/
     void Renderer::Render()
     {
         float ClearColor[4] = { 0.0f, 0.125f, 0.6f, 1.0f }; // RGBA
         m_immediateContext->ClearRenderTargetView(m_renderTargetView.Get(), ClearColor);
 
+        m_immediateContext->VSSetShader(m_vertexShader.Get(), nullptr, 0);
+        m_immediateContext->PSSetShader(m_pixelShader.Get(), nullptr, 0);
+        m_immediateContext->Draw(3, 0);
+
         m_swapChain->Present(0, 0);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderer::compileShaderFromFile
+
+      Summary:  Helper for compiling shaders with D3DCompile
+
+      Args:     PCWSTR pszFileName
+                  A pointer to a constant null-terminated string that
+                  contains the name of the file that contains the
+                  shader code
+                PCSTR pszEntryPoint
+                  A pointer to a constant null-terminated string that
+                  contains the name of the shader entry point function
+                  where shader execution begins
+                PCSTR pszShaderModel
+                  A pointer to a constant null-terminated string that
+                  specifies the shader target or set of shader
+                  features to compile against
+                ID3DBlob** ppBlobOut
+                  A pointer to a variable that receives a pointer to
+                  the ID3DBlob interface that you can use to access
+                  the compiled code
+
+      Returns:  HRESULT
+                  Status code
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    /*--------------------------------------------------------------------
+      TODO: Renderer::compileShaderFromFile definition (remove the comment)
+    --------------------------------------------------------------------*/
+    HRESULT Renderer::compileShaderFromFile(_In_ PCWSTR pszFileName, _In_ PCSTR pszEntryPoint, _In_ PCSTR szShaderModel, _Outptr_ ID3DBlob** ppBlobOut)
+    {
+        if (!pszFileName || !pszEntryPoint || !szShaderModel || !ppBlobOut)
+            return E_INVALIDARG;
+        HRESULT hr = S_OK;
+        DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+#if defined(DEBUG) || defined(_DEBUG)
+        dwShaderFlags |= D3DCOMPILE_DEBUG;
+        dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+        ID3DBlob* pErrorBlob = nullptr;
+        hr = D3DCompileFromFile(
+            pszFileName,
+            nullptr,
+            nullptr,
+            pszEntryPoint,
+            szShaderModel,
+            dwShaderFlags,
+            0u,
+            ppBlobOut,
+            &pErrorBlob
+        );
+
+        if (FAILED(hr))
+        {
+            if (pErrorBlob)
+            {
+                OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+                pErrorBlob->Release();
+            }
+            return hr;
+        }
+        if (pErrorBlob) pErrorBlob->Release();
+
+        return hr;
     }
 }
